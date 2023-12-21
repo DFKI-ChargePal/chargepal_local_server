465f5dbdee5f66a61dede0240d665b7b6da8ac5c
Alexander Sung <alexander.sung@dfki.de>
2023-12-21 15:22:46 +0100

    Simplify network setup by making ping a service on the local server

--- src/chargepal_local_server/local_server.py
+++ src/chargepal_local_server/local_server.py
@@ -1,19 +1,10 @@
-from typing import Any, Dict, Iterator, Type, TypeVar
+from typing import Any, Iterator, Type, TypeVar
 from concurrent.futures import ThreadPoolExecutor
-from grpc._channel import _InactiveRpcError
 import grpc
 import logging
 from chargepal_local_server import local_server_pb2_grpc
-from chargepal_local_server.local_server_pb2 import (
-    EmptyMessage,
-    PortInfo,
-    Pos,
-    TextMessage,
-)
-from chargepal_local_server.local_server_pb2_grpc import (
-    LocalServerServicer,
-    RobotClientStub,
-)
+from chargepal_local_server.local_server_pb2 import EmptyMessage, Pos, TextMessage
+from chargepal_local_server.local_server_pb2_grpc import LocalServerServicer
 
 
 T = TypeVar("T", bound="LocalServer")
@@ -26,7 +17,6 @@ class LocalServer(LocalServerServicer):
     def __init__(self, grpc_server: grpc.Server) -> None:
         super().__init__()
         self.grpc_server = grpc_server  # Note: Must be refereced for persistence.
-        self.robot_client_stubs: Dict[int, RobotClientStub] = {}
 
     @classmethod
     def serve(cls: Type[T]) -> T:
@@ -38,10 +28,8 @@ class LocalServer(LocalServerServicer):
         grpc_server.start()
         return server
 
-    def ConnectClient(self, request: PortInfo, context: Any) -> EmptyMessage:
-        """Connect to robot client on the port provided."""
-        channel = grpc.insecure_channel(f"{self.IP_ADDRESS}:{request.port}")
-        self.robot_client_stubs[request.port] = RobotClientStub(channel)
+    def Ping(self, request: EmptyMessage, context: Any) -> EmptyMessage:
+        """Ping between robot client and local server."""
         return EmptyMessage()
 
     def SendTextMessage(self, request: TextMessage, context: Any) -> EmptyMessage:
@@ -53,14 +41,3 @@ class LocalServer(LocalServerServicer):
         Let robot client update its position and get position updates
         for the other robots from the server.
         """
-
-    def ping_robots(self) -> None:
-        try:
-            for port, stub in self.robot_client_stubs.items():
-                stub.Ping(EmptyMessage())
-        except _InactiveRpcError:
-            del self.robot_client_stubs[port]
-            self.on_inactive_rpc_error()
-
-    def on_inactive_rpc_error(self) -> None:
-        pass
--- src/chargepal_local_server/text_server_example.py
+++ src/chargepal_local_server/text_server_example.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 from typing import Any
-import time
 from chargepal_local_server.local_server import LocalServer
-from chargepal_local_server.local_server_pb2 import EmptyMessage, PortInfo, TextMessage
+from chargepal_local_server.local_server_pb2 import EmptyMessage, TextMessage
 
 
 class TextServer(LocalServer):
@@ -11,33 +10,11 @@ class TextServer(LocalServer):
         print(request.text)
         return result
 
-    def ConnectClient(self, request: PortInfo, context: Any) -> EmptyMessage:
-        result = super().ConnectClient(request, context)
-        print(f"Robot client connected on port {request.port}.")
-        self.print_robot_ports()
-        return result
-
-    def on_inactive_rpc_error(self) -> None:
-        print("Connection to robot client lost.")
-        self.print_robot_ports()
-
-    def print_robot_ports(self):
-        print(f" Robot ports: [{', '.join(map(str, self.robot_client_stubs.keys()))}]")
-
 
 if __name__ == "__main__":
-    PING_INTERVAL = 1.0
-
-    print(
-        "Listening for messages and occassionally sending one (press <Ctrl-C> to stop) ..."
-    )
-    next_time = time.time() + PING_INTERVAL
+    print("Listening for messages (press <Ctrl-C> to stop) ...")
     try:
         server = TextServer.serve()
-        while True:
-            time.sleep(next_time - time.time())
-            next_time += PING_INTERVAL
-            if server.robot_client_stubs:
-                server.ping_robots()
+        server.grpc_server.wait_for_termination()
     except KeyboardInterrupt:
         print()
