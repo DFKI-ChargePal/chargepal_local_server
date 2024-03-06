44e489e766c9908c3d925cc5d8f01f3245c12242
Alexander Sung <alexander.sung@dfki.de>
2024-03-06 16:32:18 +0100

    Add command line parameters to demo loop script

--- src/chargepal_local_server/loop_demo.py
+++ src/chargepal_local_server/loop_demo.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-import debug_ldb
+import sys
 import time
+import debug_ldb
 from create_ldb_orders import create_sample_booking
 
 
-OPERATION_TIME = 20.0
-LOOP_TIME = 30.0
+DEFAULT_OPERATION_TIME = 20.0
+DEFAULT_LOOP_TIME = 10.0
 
 
-def loop() -> None:
+def loop(operation_time: float, loop_time: float) -> None:
     while True:
         create_sample_booking()
-        time.sleep(OPERATION_TIME)
+        time.sleep(operation_time)
         while True:
             status = debug_ldb.select("charging_session_status FROM orders_in")[-1][0]
             if status == "plugin_success":
@@ -20,8 +21,16 @@ def loop() -> None:
                     " WHERE charging_session_status = 'plugin_success'"
                 )
                 break
-        time.sleep(LOOP_TIME)
+        time.sleep(loop_time)
 
 
 if __name__ == "__main__":
-    loop()
+    operation_time = DEFAULT_OPERATION_TIME
+    loop_time = DEFAULT_LOOP_TIME
+    if len(sys.argv) > 1 and sys.argv[1].isnumeric():
+        operation_time = float(sys.argv[1])
+        print(f"Using operation time: {operation_time}")
+        if len(sys.argv) > 2 and sys.argv[2].isnumeric():
+            loop_time = float(sys.argv[2])
+            print(f"Using loop time: {loop_time}")
+    loop(operation_time, loop_time)
