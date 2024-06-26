b79e4582d184b7551d501f4fc5affad6b8c59705
gparthasarathy <gurunatraj.parthasarathy@dfki.de>
2024-06-26 13:10:14 +0200

    updates ldb from a packed string containing table, row name, column names and values
    Signed-off-by: gparthasarathy <gurunatraj.parthasarathy@dfki.de>

--- src/chargepal_local_server/update_ldb.py
+++ src/chargepal_local_server/update_ldb.py
@@ -7,133 +7,36 @@ current_directory = os.getcwd()
 
 
 def update(packaged_strings: List[str]) -> bool:
-    
     with sqlite3.connect(current_directory + "/db/ldb.db") as ldb_connection:
         ldb_cursor = ldb_connection.cursor()
 
-        for package in packaged_strings:
-            
-            table_name, string_rdb_data = package.split(":", 1)
-            rows_data: List[Tuple[object, ...]] = ast.literal_eval(string_rdb_data)
-         
-            table_name = (
-                "cart_info" if table_name == "battery_action_info" else table_name
-            )
-            ldb_cursor.execute(
-                f"SELECT sql FROM sqlite_master WHERE type='table' AND name=?",
-                (table_name,),
-            )
-            table_definition = ldb_cursor.fetchone()[0]
-            # Extract column names from the table definition
-            column_names = [
-                part.split()[0]
-                for part in table_definition.split("(")[1].split(",")
-                if "rowid" not in part
-            ]
-            column_set = set(column_names)
+        for data_package_str in packaged_strings:
+            data_package: List[Tuple[object, ...]] = ast.literal_eval(data_package_str)
+            for table_name, table_data in data_package.items():
+                if table_name == "battery_action_info":
+                    table_name = "cart_info"
 
-            if table_name == "robot_info":
-                for row in rows_data:
-                    r_name = row[0]
-                    row_data = dict(zip(column_names, row))
+                for row_name, row_data in table_data.items():
+                    update_columns = []
+                    update_values = []
+                    for column_name, value in row_data.items():
 
-                    # Only update columns that exist in both the table and row data
-                    update_columns = [col for col in column_names if col in row_data]
-                    update_values = [row_data[col] for col in update_columns]
+                        update_columns.append(column_name)
+                        update_values.append(value)
 
-                    set_columns = ", ".join([f"{col} = ?" for col in update_columns])
-
-                    ldb_cursor.execute(
-                        f"SELECT * FROM {table_name} WHERE name = ?", (r_name,)
-                    )
-                    result_robot = ldb_cursor.fetchone()
-
-                    if result_robot:
-                        ldb_cursor.execute(
-                            f"UPDATE {table_name} SET {set_columns} WHERE name = ?",
-                            update_values + [r_name],
-                        )
-
-            elif table_name == "cart_info":
-                for row in rows_data:
-                    c_name = row[0]
-                    row_data = dict(zip(column_names, row))
-                    print(row_data)
-                    # Only update columns that exist in both the table and row data
-                    update_columns = [col for col in column_names if col in row_data]
-                    update_values = [row_data[col] for col in update_columns]
-
-                    set_columns = ", ".join([f"{col} = ?" for col in update_columns])
-                    print(set_columns)
-                    ldb_cursor.execute(
-                        f"SELECT * FROM {table_name} WHERE name = ?", (c_name,)
-                    )
-                    result_cart = ldb_cursor.fetchone()
-
-                    if result_cart:
-                        ldb_cursor.execute(
-                            f"UPDATE {table_name} SET {set_columns} WHERE name = ?",
-                            update_values + [c_name],
-                        )
-
-        ldb_connection.commit()
-
-    return True
-
-
-""" 
-
-def update(string_rdb_data: str) -> None:
-    rows_data: List[Tuple[object, ...]] = ast.literal_eval(string_rdb_data)
-    with sqlite3.connect(current_directory + "/db/ldb.db") as ldb_connection:
-        ldb_cursor = ldb_connection.cursor()
-
-        ldb_cursor.execute(
-            f"SELECT sql FROM sqlite_master WHERE type='table' AND name=?",
-            (table_name,),
-        )
-        table_definition = ldb_cursor.fetchone()[0]
-        # Extract column names from the table definition
-        column_names = [
-            part.split()[0]
-            for part in table_definition.split("(")[1].split(",")
-            if "rowid" not in part
-        ]
-        set_columns = ", ".join([f"{column_name} = ?" for column_name in column_names])
-
-        if table_name == "robot_info":
-            for row in rows_data:
-                r_name = row[0]
+                set_columns = ", ".join([f"{col} = ?" for col in update_columns])
 
                 ldb_cursor.execute(
-                    f"SELECT * FROM {table_name} WHERE name = ?", (r_name,)
+                    f"SELECT * FROM {table_name} WHERE name = ?", (row_name,)
                 )
-                result_robot = ldb_cursor.fetchone()
+                result = ldb_cursor.fetchone()
 
-                if result_robot:
+                if result:
                     ldb_cursor.execute(
                         f"UPDATE {table_name} SET {set_columns} WHERE name = ?",
-                        row[0:] + (row[0],),
+                        update_values + [row_name],
                     )
 
-        elif table_name == "cart_info":
-            for row in rows_data:
-                c_name = row[0]
-
-                ldb_cursor.execute(
-                    f"SELECT * FROM {table_name} WHERE name = ?", (c_name,)
-                )
-                result_cart = ldb_cursor.fetchone()
-
-                if result_cart:
-                    ldb_cursor.execute(
-                        f"UPDATE {table_name} SET {set_columns} WHERE name = ?",
-                        row[0:] + (row[0],),
-                    )
-        elif table_name == "battery_actions_info" :
-            pass
-
-        ldb_connection.commit()
+            ldb_connection.commit()
 
     return True
- """
