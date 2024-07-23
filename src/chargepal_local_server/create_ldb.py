d5f6a6a21daaa5249cc8d08beb93de645f79442a
gparthasarathy <gurunatraj.parthasarathy@dfki.de>
2024-07-23 14:30:47 +0200

    adds dummy envi values
    Signed-off-by: gparthasarathy <gurunatraj.parthasarathy@dfki.de>

--- src/chargepal_local_server/create_ldb.py
+++ src/chargepal_local_server/create_ldb.py
@@ -71,10 +71,45 @@ def main(robots_count: int = 1, carts_count: int = 1):
 
     
     cursor.execute("DELETE FROM env_info")
+    robot_names = []
+    cart_names = []
+    bws_names = []
+    rbs_names = []
+    ads_names = ['ADS_1']
+    bcs_names = ['BCS_1']
+    for number in range(1, robots_count + 1):
+        robot_names.append(f"ChargePal{number}")
+        rbs_names.append(f"RBS_{number}")
+    for number in range(1, carts_count + 1):
+        cart_names.append(f"BAT_{number}")
+        bws_names.append(f"BWS_{number}")
+        
     
+    env_info = ("robot_names",f"{robot_names}",len(robot_names))
+    excute_env_info(cursor,env_info)
+    
+    env_info = ("rbs",f"{rbs_names}",len(rbs_names))
+    excute_env_info(cursor,env_info)
+    
+    env_info = ("ads",f"{ads_names}",len(ads_names))
+    excute_env_info(cursor,env_info)
+    
+    env_info = ("bcs",f"{bcs_names}",len(bcs_names))
+    excute_env_info(cursor,env_info)
+    
+    env_info = ("cart_names",f"{cart_names}",len(cart_names))
+    excute_env_info(cursor,env_info)
+    
+    env_info = ("bws",f"{bws_names}",len(bws_names))
+    excute_env_info(cursor,env_info)
     conn.commit()
     conn.close()
 
-
+def excute_env_info(cursor: sqlite3.Cursor, info: tuple):
+    cursor.execute(
+        "INSERT INTO env_info (name, value, count) VALUES (?,?,?)",
+        info,
+    )
+    
 if __name__ == "__main__":
     main()
